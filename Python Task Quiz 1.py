102c2725b196162d05e554bade0dd8211b5ea674
Brent Noble <brentnoble@hotmail.com>
2024-05-16 19:28:51 +1000

    First Commit

---
+++ Python Task Quiz 1.py
@@ -0,0 +1,16 @@
+# Python Task Quiz 1
+# 1. Prints your name, and then counts up to your favourite number squared. 
+# 2. Commit to Github.
+# 3. Create a class Engineers, 
+#   with a fixed class attribute skill equal to "problem solver" 
+#   and initial attributes name, type and years of experience 
+#   Create two different engineers (objects) and print out their attributes
+# 3a. Also change your name to be the reverse. I.e. Jim becomes Mij
+# 4. Commit this new code to your github.
+# 5. In the quiz, paste link your github repository.
+
+name = "Brent"
+print(name)
+bestNumber = 12
+for i in range(bestNumber*bestNumber):
+    print(i)
