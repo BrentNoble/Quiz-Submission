f05d91e1d770fe37bdc245f5a27b75b64bfc8ad4
Brent Noble <brentnoble@hotmail.com>
2024-05-16 20:20:07 +1000

    Added Class & Objects

--- Python Task Quiz 1.py
+++ Python Task Quiz 1.py
@@ -1,16 +1,34 @@
 # Python Task Quiz 1
-# 1. Prints your name, and then counts up to your favourite number squared. 
-# 2. Commit to Github.
-# 3. Create a class Engineers, 
-#   with a fixed class attribute skill equal to "problem solver" 
-#   and initial attributes name, type and years of experience 
-#   Create two different engineers (objects) and print out their attributes
-# 3a. Also change your name to be the reverse. I.e. Jim becomes Mij
-# 4. Commit this new code to your github.
-# 5. In the quiz, paste link your github repository.
-
+# Print my name
 name = "Brent"
 print(name)
+# Count up to my favorite number squared then commit
 bestNumber = 12
-for i in range(bestNumber*bestNumber):
+for i in range(1,bestNumber*bestNumber+1):
     print(i)
+# Create a class called engineers
+class Engineers:
+    # Class attribute
+    skill = "problem solver"
+    def __init__(self, name: str , type: str, experience: int) -> None:
+        # Instance attributes
+        self.name = name
+        self.type = type
+        self.experience = experience
+# Create two different egnineers (objects)
+engineer1 = Engineers(name = "Brad", type="Electrical", experience=10)
+engineer2 = Engineers(name = "Karen", type = "Civil", experience = 15)
+# Print attributes
+print(engineer1.skill)
+print(engineer1.name)
+print(engineer1.type)
+print(engineer1.experience)
+
+print(engineer2.skill)
+print(engineer2.name)
+print(engineer2.type)
+print(engineer2.experience)
+# Reverse name
+reverseName = name[::-1]
+print(reverseName)
+# Commit and post link
